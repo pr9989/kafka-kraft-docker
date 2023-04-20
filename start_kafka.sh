94581e9495218928d40e4be922f07f0b103a9e2f
pr9989 <131358247+pr9989@users.noreply.github.com>
2023-04-20 16:17:24 +0530

    startup config changed

--- start_kafka.sh
+++ start_kafka.sh
@@ -30,4 +30,4 @@ fi
 
 uuid=$(/opt/kafka/bin/kafka-storage.sh random-uuid)
 /opt/kafka/bin/kafka-storage.sh format -t $uuid -c /opt/kafka/config/kraft/server.properties
-/opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/kraft/server.properties
+# /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/kraft/server.properties
